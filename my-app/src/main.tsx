a78881babda0651d766c0e0d48b84eff7e655f92
Muhammad Nour Dabash <muhammad.nour@rightcode.io>
2025-05-15 19:42:50 +0300

    Done ^_^

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,16 +5,11 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import "./i18n";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        {/* <I18nextProvider i18n={i18n}> */}
         <App />
-        {/* </I18nextProvider> */}
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
--- my-app/src/pages/LoginPage.tsx
+++ my-app/src/pages/LoginPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import supabase from "../services/supabaseClient";
